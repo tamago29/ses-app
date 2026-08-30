dddca09353d5e60d4a304b52f5541cfb6364cfd6
冨田奈央 <nao12298@icloud.com>
2026-08-30 15:48:33 +0900

    feat: Add delete button to daily log list

--- src/main/java/com/example/sesapp/controller/DailyLogController.java
+++ src/main/java/com/example/sesapp/controller/DailyLogController.java
@@ -1,5 +1,6 @@
 package com.example.sesapp.controller;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.sesapp.entity.DailyLog;
 import com.example.sesapp.entity.TaskCategory;
@@ -94,14 +96,17 @@ public class DailyLogController {
 
     // 登録処理
     @PostMapping("/register")
-    public String register(
-            @ModelAttribute DailyLogForm form) {
+    public String register(@ModelAttribute DailyLogForm form) {
 
-        dailyLogService.saveAll(
-                form.getItems(),
-                form.getWorkDate()
-        );
+        dailyLogService.saveAll(form.getItems(),form.getWorkDate());
 
         return "redirect:/daily-log/list";
+    }
+    
+    // 削除処理
+    @PostMapping(value = "/delete", params = "delete")
+    public String deleteRow(@RequestParam("delete") LocalDate workDate) {
+    	dailyLogService.deleteById(workDate);
+    	return "redirect:/daily-log/list";
     }
 }
--- src/main/java/com/example/sesapp/repository/TaskCategoryRepository.java
+++ src/main/java/com/example/sesapp/repository/TaskCategoryRepository.java
@@ -13,5 +13,5 @@ public interface TaskCategoryRepository extends JpaRepository<TaskCategory, Inte
 	// 「特定のユーザーのカテゴリ一覧のみ」をNo順で全て取得するメソッド
     List<TaskCategory> findByUserIdOrderByNo(Integer userId);
     
-    int deleteByIdAndUserId(Integer id, Integer userId);
+    void deleteByIdAndUserId(Integer id, Integer userId);
 }
